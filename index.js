7912ccfc09211faced6d3f83a4b65a406e5bcdf6
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-02-19 11:28:56 +0000

    created template for README generator

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// Using inquirer.prompt, create an array aof questions to ask the user
+// for multiple choice questions like License use a list 
+
+const init = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+
 // array of questions for user
 const questions = [
 
@@ -23,13 +60,9 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+// function init() {
 
 }
 
 // function call to initialize program
 init();
-
-const util = require('util');
-
-const writeFileAsync = util.promisify(fs.writeFile);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,66 @@
+//create a README.md template with spaces fro the user input to be added.
+// The contents section needs to be selectable using * [Installation](#installation)
+
+//  * Description 
+// * Table of Contents 
+// * Installation 
+// * Usage 
+// * License 
+// * Contributing 
+// * Tests 
+// * Questions
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+
+  `#  ${answers.title}  ${answers.badge}
+
+  ## Description
+  
+  ${answers.description}
+  
+
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  
+  ${answers.installation}
+  
+  
+  ## Usage 
+  
+  ${answers.usage}
+  
+
+  ## License
+  
+  ${answers.usage}
+  
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  
+  ## Tests
+  
+  ${answers.tests}
+  
+  ## Questions
+  
+  ${answers.questions}
+
+
+  ---`
+
   return `# ${data.title}
 
 `;
